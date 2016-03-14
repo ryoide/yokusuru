729f0156a401d038b128072f2237c262f15d1231
ryoide <deryou0426@gmail.com>
2016-03-14 16:53:19 +0900

    accessor in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  #accessor
+  attr_accessor :team_name
 end
