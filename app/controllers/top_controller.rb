63a65bfa377a8a7815e14d8f36f3d0d2e57094e4
ryoide <deryou0426@gmail.com>
2016-03-15 22:58:49 +0900

    if user_signed_in?=true, redirect_to home

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,14 @@
 class TopController < ApplicationController
-
+		before_action :home
 	def index
+	end
+
+
+	private
+
+	def home
+		if user_signed_in?
+			redirect_to controller: 'home', action: 'index' and return
+		end
 	end
 end
