dbed918647d9169f9eff8a8de4ed02811544b3a0
ryoide <deryou0426@gmail.com>
2016-03-15 23:22:13 +0900

    team routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'top#index'
   get  'home' => 'home#index'
   resources :users, :only => [:show, :edit, :update]
+  resources :teams, :only => [:show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
