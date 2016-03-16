adbf4df2e8346abe5638eb2758d6402b79e4f377
ryoide <deryou0426@gmail.com>
2016-03-16 20:40:35 +0900

    create create problem page

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,4 +1,15 @@
 class ProblemsController < ApplicationController
 	def new
+		@problem = Problem.new
+	end
+
+	def create
+		Problem.create(problem: create_params[:problem], plan: create_params[:plan], user_id: current_user.id, team_id: current_user.team_id)
+    	redirect_to :root and return
+	end
+
+	private
+	def create_params
+		params.require(:problem).permit(:problem, :plan)
 	end
 end
