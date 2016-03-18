6d0dad3b1bfaba774619639f612205d5c71789fc
ryoide <deryou0426@gmail.com>
2016-03-18 21:39:55 +0900

    put like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+
+	def like
+		problem = Problem.find(params[:problem_id])
+		like = current_user.likes.build(problem_id: problem.id)
+		like.save
+		redirect_to :root and return
+	end
+
+	def unlike
+	end
 end
