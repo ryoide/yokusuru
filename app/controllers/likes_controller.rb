0a0e0f3e859ce8b1914315b9284cd70ed6dd35be
ryoide <deryou0426@gmail.com>
2016-03-18 22:33:59 +0900

    change like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,18 @@
 class LikesController < ApplicationController
 
 	def like
+		@problem = Problem.find(params[:problem_id])
+
 		problem = Problem.find(params[:problem_id])
 		like = current_user.likes.build(problem_id: problem.id)
 		like.save
-		redirect_to :root and return
 	end
 
 	def unlike
+		@problem = Problem.find(params[:problem_id])
+		
 		problem = Problem.find(params[:problem_id])
 		like = current_user.likes.find_by(problem_id: problem.id)
 		like.destroy
-		redirect_to :root and return
 	end
 end
