0171ad10d37a32a2184f156a6972b7e5f131d815
ryoide <deryou0426@gmail.com>
2016-03-25 16:06:00 +0900

    create contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,6 @@
+class ContactMailer < ApplicationMailer
+	default from: "deryou0426@gmail.com"
+
+	def post_email
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :ranking, :only => [:show]
   post 'like/:problem_id' => 'likes#like', as: 'like'
   delete 'unlike/:problem_id' => 'likes#unlike', as: 'unlike'
+  resources :contact, :only => [:create, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
