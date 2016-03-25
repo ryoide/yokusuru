0844588cf9d2639dfd769f51812955b226646043
ryoide <deryou0426@gmail.com>
2016-03-25 17:57:26 +0900

    change mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 	default from: "deryou0426@gmail.com"
 
 	def post_email(post)
-		@subject = post.subject
-		mail to: "deryou0426@gmail.com", subject: "@subject"
+		@post = post
+		mail to: "deryou0426@gmail.com", subject: "#{@post.subject}"
 	end
 end
