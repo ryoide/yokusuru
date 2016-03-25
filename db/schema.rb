876e0047d224281cc8d1490e2e818ca7841144d3
ryoide <deryou0426@gmail.com>
2016-03-25 15:44:41 +0900

    create contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318113310) do
+ActiveRecord::Schema.define(version: 20160325063832) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "mail",       limit: 255
+    t.string   "subject",    limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
