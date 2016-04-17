f78a4f1873eda9099792e8304b26ad800f6e9ab2
ryoide <deryou0426@gmail.com>
2016-04-17 15:59:34 +0900

    add image in team

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325063832) do
+ActiveRecord::Schema.define(version: 20160417065612) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -41,11 +41,15 @@ ActiveRecord::Schema.define(version: 20160325063832) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "problem_id", limit: 4
-    t.text     "detail",     limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name",               limit: 255
+    t.integer  "problem_id",         limit: 4
+    t.text     "detail",             limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
