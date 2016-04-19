73ba3f4dd10038d255bc34468db492ea7404b292
ryoide <deryou0426@gmail.com>
2016-04-19 15:24:34 +0900

    team image column

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ActiveRecord::Base
 	has_many :users
 	has_many :problems, ->{ order("updated_at DESC") }
+
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}
+ 	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 end
