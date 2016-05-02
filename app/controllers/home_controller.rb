53619439f794a8b09df177a13108faa3dc94fa58
ryoide <deryou0426@gmail.com>
2016-05-02 16:33:53 +0900

    add js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
 	def index
 		@problems = current_user.team.problems.where(done: 0)
+		@proposals = current_user.team.problems.where(done: 1)
 	end
 
 	def doing
